37ce97d2efcbe60926ccea82bc1326c28a8523ca
Nitin Goley <113915570+nitingoley@users.noreply.github.com>
2024-12-09 18:33:21 +0530

    Update useOrderStore.ts

--- client/src/store/useOrderStore.ts
+++ client/src/store/useOrderStore.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
-const API_END_POINT = "http://localhost:4000/api/v1/order";
+const API_END_POINT = "https://food-mern-stack.onrender.com/api/v1/order";
 axios.defaults.withCredentials = true;
 
 export const useOrderStore = create<OrderState>()(
